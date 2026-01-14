0c2ff5ed4024cffa06e0c27e4fc6a61609128337
andrew@tainan <dimsplendid@gmail.com>
2026-01-15 00:08:45 +0800

    ch 5: recursive example complete

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -2,4 +2,21 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Array (uncons)
+import Data.Maybe (Maybe(..))
+
 -- Note to reader: Add your solutions to this file
+
+isEven :: Int -> Boolean
+isEven 0 = true
+isEven 1 = false
+isEven n
+    | n < 0     = isEven (-n)
+    | otherwise = isEven (n-2)
+
+countEven :: Array Int -> Int
+countEven arr = case uncons arr of
+    Nothing -> 0
+    Just {head: x, tail: xs}
+        | isEven(x) -> countEven xs + 1
+        | otherwise -> countEven xs
